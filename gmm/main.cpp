eaae20ca42dff17abe90c1634fca73dcb1b21c68
Luis Vitorio Cargnini <lvcargnini@gmail.com>
2017-07-25 16:40:43 -0700

    On branch master  Changes to be committed: 	new file:   gmm/main.cpp

---
+++ gmm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gmm
+//
+//  Created by Luis Vitorio Cargnini on 7/25/17.
+//  Copyright © 2017 Luis Vitorio Cargnini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
